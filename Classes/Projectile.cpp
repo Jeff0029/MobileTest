ebf5af1168e63cf43e8bfb734eed9f90cc49a825
Jeff0029 <jeanfrancoisvienneau@gmail.com>
2014-10-20 18:10:57 -0400

    Added slingshot capability with rotation and movement restriction.  added Projectile creation with varying throwing forces.

---
+++ Classes/Projectile.cpp
@@ -0,0 +1,9 @@
+//
+//  Projectile.cpp
+//  ArcticTest
+//
+//  Created by Jean-Francois Vienneau on 2014-10-18.
+//
+//
+
+#include "Projectile.h"
---
+++ Classes/Projectile.h
@@ -0,0 +1,14 @@
+//
+//  Projectile.h
+//  ArcticTest
+//
+//  Created by Jean-Francois Vienneau on 2014-10-18.
+//
+//
+
+#ifndef __ArcticTest__Projectile__
+#define __ArcticTest__Projectile__
+
+#include <stdio.h>
+
+#endif /* defined(__ArcticTest__Projectile__) */
